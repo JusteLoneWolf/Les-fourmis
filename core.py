172e91b595fcf85006870d1eb410f34330e44ff8
zechaos031 <delrieu.adrien@gmail.com>
2020-03-11 18:55:50 +0100

    [ADD] Addings screen & QUIT event

--- core.py
+++ core.py
@@ -1,3 +1,15 @@
 import pygame
-
+from Class import Player
 pygame.init()
+
+pygame.display.set_caption('Les fourmis')
+fenetre = pygame.display.set_mode((640, 480))
+
+run = True
+while run:
+    print(Player.Player().holding)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            pygame.quit()
+            print('Fermeture... ')
