db15076ea966b0baadd308caefab94944f0725ad
zechaos031 <delrieu.adrien@gmail.com>
2020-03-11 22:10:14 +0100

    [ADD] Add Game Class

---
+++ Class/Game.py
@@ -0,0 +1,7 @@
+from Class.Player import Player # Appel de la class Player
+import pygame
+
+class Game:
+    def __init__(self):
+        self.player = Player() # Initialise a la class player
+        self.press = {} # Les boutons
