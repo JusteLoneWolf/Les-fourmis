b11be3c234f25a726fa93fe21512007b5c4146ca
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2020-04-20 15:14:26 +0200

    Delete Nouriture.py

--- Class/Nouriture.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Nouriture:
-    def __init__(self):
-        self.image= pygame.image.load('./asset/nourriture/fruit.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (50, 50))  # resize de la fourmie
-        self.Imagerect = self.image.get_rect(center=(150, 150))
-        self.Imagerect.x = 350  # Cadrage de la fourmie
-        self.Imagerect.y = 400  # Cadrage de la fourmie
